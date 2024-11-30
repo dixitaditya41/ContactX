62ffc9fae0d4d08d89b39782d6e321fb53546f00
Aditya dixit <85583554+dixitaditya41@users.noreply.github.com>
2024-12-01 05:03:54 +0530

    Update createToken.js

--- server/utils/createToken.js
+++ server/utils/createToken.js
@@ -9,7 +9,7 @@ const createToken = (res,userId) => {
     res.cookie("jwt", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'Strict',
+        sameSite: 'None',
         maxAge: 30 * 24 * 60 * 60 * 1000
     });
 
